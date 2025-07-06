6d7a044dd9cbcaeb08545c5fb8d4a7a4b3d068d9
rawatashutosh26 <143058897+rawatashutosh26@users.noreply.github.com>
2025-07-06 23:26:42 +0530

    Time: 137 ms (66.30%) | Memory: 41.4 MB (95.09%) - LeetSync

---
+++ 79-word-search/word-search.java
@@ -0,0 +1,44 @@
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        if (board == null || board.length == 0 || board[0].length == 0) {
+            return false;
+        }
+        
+        int rows = board.length;
+        int cols = board[0].length;
+        
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (board[i][j] == word.charAt(0)) {
+                    if (dfs(board, i, j, word, 0)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+    
+    private boolean dfs(char[][] board, int i, int j, String word, int index) {
+        if (index == word.length()) {
+            return true;
+        }
+        
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || 
+            board[i][j] != word.charAt(index)) {
+            return false;
+        }
+        
+        char temp = board[i][j];
+        board[i][j] = '#';
+        
+        boolean found = dfs(board, i + 1, j, word, index + 1) ||
+                        dfs(board, i - 1, j, word, index + 1) ||
+                        dfs(board, i, j + 1, word, index + 1) ||
+                        dfs(board, i, j - 1, word, index + 1);
+        
+        board[i][j] = temp;
+        
+        return found;
+    }
+}
