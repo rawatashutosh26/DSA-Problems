c9fabdc501971fb521406073ecd1d02e6bac15af
rawatashutosh26 <143058897+rawatashutosh26@users.noreply.github.com>
2025-07-19 11:58:05 +0530

    Time: 1 ms (71.70%) | Memory: 41.3 MB (24.65%) - LeetSync

--- 29-divide-two-integers/divide-two-integers.java
+++ 29-divide-two-integers/divide-two-integers.java
@@ -1,6 +1,5 @@
 class Solution {
     public int divide(int dividend, int divisor) {
-        // Handle overflow
         if (dividend == Integer.MIN_VALUE && divisor == -1) {
             return Integer.MAX_VALUE;
         }
